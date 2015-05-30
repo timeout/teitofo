c08345dd9d573fa065e82e55114aa877d6f19417
Joe Gain <joe.gain@gmail.com>
2015-05-30 14:50:37 +0200

    added spec ArticlePart#add_part

--- spec/lib/teitofo/article_part_spec.rb
+++ spec/lib/teitofo/article_part_spec.rb
@@ -1,18 +1,25 @@
 require 'teitofo/article_part'
 
 RSpec.describe TeiToFo::ArticlePart do
-  let(:articlePart) { TeiToFo::ArticlePart.new }
+  let(:article_part) { TeiToFo::ArticlePart.new }
   let(:article_name) { 'article' }
   describe '#name=' do
     it 'sets a name' do
-      articlePart.name = article_name
+      article_part.name = article_name
     end
   end
 
   describe '#name' do
     it 'gets a name' do
-      articlePart.name = article_name
-      expect(articlePart.name).to eq(article_name)
+      article_part.name = article_name
+      expect(article_part.name).to eq(article_name)
+    end
+  end
+
+  describe '#add_part' do
+    let(:article_sub_part) { TeiToFo::ArticlePart.new }
+    it 'adds an article part to an article part' do
+      article_part.add_part(article_sub_part)
     end
   end
 end
