4aefb8cf575ba1401832efcc1309ea17e95c6d03
Joe Gain <joe.gain@gmail.com>
2015-05-30 14:15:25 +0200

    ArticlePart attr_accessor for @name

---
+++ spec/lib/teitofo/article_part.rb
@@ -0,0 +1,16 @@
+Rspec.describe TeiToFo::ArticlePart do
+  let(:articlePart) { TeiToFo::ArticlePart.new }
+  let(:article_name) { 'article' }
+  describe '#name=' do
+    it 'sets a name' do
+      articlePart.name = article_name
+    end
+  end
+
+  describe '#name' do
+    it 'gets a name' do
+      articlePart.name = article_name
+      expect(articlePart.name).to eq(article_name)
+    end
+  end
+end
