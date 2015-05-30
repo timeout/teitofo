c4fff94a5e657fd034386766b1b4ff1c4e658f50
Joe Gain <joe.gain@gmail.com>
2015-05-30 15:07:58 +0200

    added skeleton of ArticleMachine class

--- lib/teitofo/article_handler.rb
+++ lib/teitofo/article_handler.rb
@@ -1,12 +1,22 @@
 require 'ox'
 
+require 'teitofo/article_machine'
+
 class ArticleHandler < Ox::Sax
+
+  def initialize
+    @machine = TeiToFo::ArticleMachine.new
+  end
+
   def element_start(name)
+    @machine.on_element_state(name)
   end
 
   def element_end(name)
+    @machine.on_element_end(name)
   end
 
   def text(value)
+    @machine.on_text(value)
   end
 end
---
+++ lib/teitofo/article_machine.rb
@@ -0,0 +1,12 @@
+class ArticleMachine
+
+  def on_element_start(name)
+  end
+
+  def on_element_end(name)
+  end
+
+  def on_text(value)
+  end
+
+end
