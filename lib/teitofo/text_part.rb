1b73d61f120015e9f29ddc96a5545b0e17d8942c
Joe Gain <joe.gain@gmail.com>
2015-06-02 12:15:29 +0200

    #to_s

--- lib/teitofo/text_part.rb
+++ lib/teitofo/text_part.rb
@@ -1,11 +1,18 @@
+require 'teitofo/exceptions'
+
 module TeiToFo
   class TextPart
 
     def initialize(text, type = :text)
+      raise EmptyTextPartError.new("error: #{self}") if text.nil? || text.empty?
       @text, @type = text, type
     end
 
     attr_reader :type, :text
+
+    def to_s
+      "#{type}: #{text}"
+    end
 
   end
 end
--- spec/lib/teitofo/text_part_spec.rb
+++ spec/lib/teitofo/text_part_spec.rb
@@ -1,4 +1,5 @@
 require 'teitofo/text_part'
+require 'teitofo/exceptions'
 
 RSpec.describe TeiToFo::TextPart do
   let(:text_part) { TeiToFo::TextPart.new('This is some text') }
@@ -16,6 +17,25 @@ RSpec.describe TeiToFo::TextPart do
   describe '#text' do
     it 'has some text' do
       expect(text_part.text).to include('some text')
+    end
+
+    it 'raises a EmptyTextPartError' do
+      expect{ TeiToFo::TextPart.new('') }.to raise_error(TeiToFo::EmptyTextPartError)
+    end
+
+    it 'raises a EmptyTextPartError' do
+      expect{ TeiToFo::TextPart.new(String.new) }.to raise_error(TeiToFo::EmptyTextPartError)
+    end
+  end
+
+  describe '#to_s' do
+    it 'creates the string: "text: This is some text"' do
+      expect(text_part.to_s).to eq('text: This is some text')
+    end
+
+    it 'creates the string: "italic: IMPORTANT"' do
+      italic_text = TeiToFo::TextPart.new('IMPORTANT', :italic)
+      expect(italic_text.to_s).to eq('italic: IMPORTANT')
     end
   end
 
